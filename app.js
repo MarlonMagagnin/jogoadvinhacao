0e1cb41cf9e40521ca89b661cfaa564b7fb1f95d
Marlon Magagnin <MarlonMagagnin@users.noreply.github.com>
2022-09-23 14:32:57 -0300

    código javascript

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const num_maior = []
+const num_menor = []
+var numero = Math.floor((Math.random() * 1000) + 1)
+var contador = 0
+document.getElementById("btn").onclick=function(){
+    if(document.getElementById("palpite").value > numero){
+        num_maior.push(document.getElementById("palpite").value)
+        document.getElementById("num_maior").innerHTML = num_maior.toString(" * ");
+        contador++
+    } else if (document.getElementById("palpite").value < numero){
+        num_menor.push(document.getElementById("palpite").value)
+        document.getElementById("num_menor").innerHTML = num_menor.toString(" * ");
+        contador++
+    } else {
+        document.getElementById("contador").innerHTML = contador
+        document.getElementById("acertou").innerHTML = numero
+    }
+}
+
+var btn = document.querySelector("#recarregar");
+btn.addEventListener("click", function() {
+    
+    location.reload();
+});
